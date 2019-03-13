256100ec806cef46e905c94f3cb3ec2f7eedaf58
Tom Straub <tom.straub@insight.com>
2019-03-13 17:04:14 -0500

    Updated to reach from local binaries.

--- build.sh
+++ build.sh
@@ -8,9 +8,17 @@ set -e
 # Optional with defaults
 AZURE_RESOURCE_GROUP=${AZURE_RESOURCE_GROUP:-"hashistack-images-rg"}
 AZURE_LOCATION=${AZURE_LOCATION:-"eastus"}
-IMAGE_NAME=${IMAGE_NAME:-"hashiconsul-vault-image"}
+IMAGE_NAME=${IMAGE_NAME:-"hashi-consul-vault-image"}
 IMAGE_VERSION=${IMAGE_VERSION:-"v0.1"}
 
+echo "Using the following values:"
+echo "  AZURE_RESOURCE_GROUP = ${AZURE_RESOURCE_GROUP}"
+echo "  AZURE_LOCATION       = ${AZURE_LOCATION}"
+echo "  IMAGE_NAME           = ${IMAGE_NAME}"
+echo "  IMAGE_VERSION        = ${IMAGE_VERSION}"
+echo ""
+
+echo "Building HashiStack image with Packer. This will take several minutes."
 if [ $(az group exists --name ${AZURE_RESOURCE_GROUP}) != "true" ]
 then
     echo "Create resource group '${AZURE_RESOURCE_GROUP}'." 
@@ -19,7 +27,6 @@ else
     echo "Resource group '${AZURE_RESOURCE_GROUP}' already exists."
 fi
 
-echo "Building HashiStack image with Packer. This will take several minutes."
 packer build \
     -var "azure_subscription_id=${ARM_SUBSCRIPTION_ID}" \
     -var "azure_client_id=${ARM_CLIENT_ID}" \
@@ -29,5 +36,6 @@ packer build \
     -var "image_name=${IMAGE_NAME}" \
     -var "image_version=${IMAGE_VERSION}" \
     -force packer.json | \
+    tee /dev/tty | \
     grep "ManagedImageId" | \
     sed 's/.* //g'
